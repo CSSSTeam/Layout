09b623440e385060831d17adb068a5be0160d478
mwawrzen <w.w@op.pl>
2020-01-26 17:44:40 +0100

    time table

--- src/app/logged-layout/lessons/lessons.component.ts
+++ src/app/logged-layout/lessons/lessons.component.ts
@@ -12,4 +12,30 @@ export class LessonsComponent implements OnInit {
   ngOnInit() {
   }
 
+  isMonday = false;
+  showMonday() {
+      if(this.isMonday==false) this.isMonday = true;
+      else this.isMonday = false;
+  }
+  isTuesday = false;
+  showTuesday() {
+      if(this.isTuesday==false) this.isTuesday = true;
+      else this.isTuesday = false;
+  }
+  isWednesday = false;
+  showWednesday() {
+      if(this.isWednesday==false) this.isWednesday = true;
+      else this.isWednesday = false;
+  }
+  isThursday = false;
+  showThursday() {
+      if(this.isThursday==false) this.isThursday = true;
+      else this.isThursday = false;
+  }
+  isFriday = false;
+  showFriday() {
+      if(this.isFriday==false) this.isFriday = true;
+      else this.isFriday = false;
+  }
+
 }
